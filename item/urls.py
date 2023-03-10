3c5a12a64a8d47c00f871a7657a2cf9777c8af15
Dev Jobalia <webdev.jobalia@gmail.com>
2023-03-10 08:51:59 +0530

    Dashboard + Edit and Delete Item

--- item/urls.py
+++ item/urls.py
@@ -7,5 +7,7 @@ app_name = 'item'
 
 urlpatterns = [
     path('<int:pk>/', views.detail, name='detail'),
-    path('new/', views.newItem, name='new')
+    path('new/', views.newItem, name='new'),
+    path('<int:pk>/delete/', views.delete, name='delete'),
+    path('<int:pk>/edit/', views.EditItem, name='edit')
 ]
--- puddle/urls.py
+++ puddle/urls.py
@@ -24,6 +24,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
-    path('items/', include('item.urls'))
+    path('items/', include('item.urls')),
+    path('dashboard/',include('Dashboard.urls'))
 
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
